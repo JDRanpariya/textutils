781266f7bbcc582928c62440578bb14b61df0c4c
JDRanpariya <jaydeepranpariya037@gmail.com>
2019-11-07 15:39:48 +0530

    backendupdate1

--- textutils/urls.py
+++ textutils/urls.py
@@ -20,5 +20,6 @@ from textutils import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='homepage'),
-    
+    path('result', views.result, name='result'),
+    path('dev', views.dev, name='dev'),
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -2,3 +2,34 @@ from django.shortcuts import render
 
 def index(request):
    return render (request, 'index.html')
+
+def dev(request):
+   return render(request,'devloper.html')
+
+
+
+def result(request):
+   raw_text = request.GET.get('text1')
+   removepanc = request.GET.get('puncb')
+   exspaceremover = request.GET.get('removeexspace')
+   analytics = request.GET.get('analytics')
+   analysed=''
+   panc = """,./?;:}{[]"!@#=$'%^&*()~`<>"""
+
+   if removepanc == 'on':
+         for char in raw_text:
+            if char not in panc:
+               analysed += char
+   elif exspaceremover == 'on':
+         for char in raw_text:
+            if char == char or char == ' ':
+               analysed += char
+   elif analytics == 'on':
+      pass
+
+   else:
+      analysed = raw_text                   
+
+   dictf={'raw_text' : analysed}
+ 
+   return render (request,'result.html', dictf)
